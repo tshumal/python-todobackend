4eac4ddf95df4684359b283eba9bb2fc62bc09dd
Lingani Tshuma <tshumal@gmail.com>
2017-07-03 16:02:21 +0200

    Added views and routing

--- src/todo/views.py
+++ src/todo/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+class TodoItemViewSet(viewsets.ModelViewSet):
+  queryset = TodoItem.objects.all()
+  serializer_class = TodoItemSerializer
+
+  def perform_create(self, serializer):
+    # Save instance to get primary key and then update URL
+    instance = serializer.save()
+    instance.url = reverse('todoitem-detail', args=[instance.pk], request=self.request)
+    instance.save()
+
+  # Deletes all todo items
+  def delete(self, request):
+    TodoItem.objects.all().delete()
+    return Response(status=status.HTTP_204_NO_CONTENT) 
