39aeeba12b099645bb9dbe5b4c889468f5115d6d
Lingani Tshuma <tshumal@gmail.com>
2017-07-04 13:49:16 +0200

    Corrected Imports in views

--- src/todo/views.py
+++ src/todo/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
 from todo.models import TodoItem
 from todo.serializers import TodoItemSerializer
+from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.reverse import reverse
+from rest_framework.decorators import list_route
+from rest_framework.response import Response
+
 
 # Create your views here.
 
